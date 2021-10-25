ba37efe882925460d46d8ff5546f046dce0bb06b
stylezdev <stylezdev@gmail.com>
2021-10-25 11:19:23 +0530

    player added from asset store

--- Prototype MPGD/Assets/Scripts/PlayerCharacter.cs
+++ Prototype MPGD/Assets/Scripts/PlayerCharacter.cs
@@ -6,17 +6,28 @@ public class PlayerCharacter : MonoBehaviour
     public int rotationSpeed = 2;
     public int health = 100;
 
+    [SerializeField]
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        if (Input.GetAxis("Vertical")!=0)
+        {
+            animator.SetBool("run", true);
+            animator.SetFloat("runspeed", Input.GetAxis("Vertical"));
+            transform.position += transform.forward * Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            animator.SetFloat("runspeed", Input.GetAxis("Vertical"));
 
+            animator.SetBool("run", false);
+        }
 
         transform.Rotate(Vector3.up* Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
 
